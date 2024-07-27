945c616cf42360293d68fd816e4eb0df9150c394
c120065 <c120065@flyfrontier.com>
2024-07-27 08:17:06 -0600

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+            <h2 className="mb-3 text-2xl font-semibold">
+         Developer Portal
+            </h2>
+      </div>
+     
+    </main>
+  );
+}
